a6bfa387c3439ebba1384b376980fb030f4bebd5
Wesley Chen <35469114+wes-chen@users.noreply.github.com>
2021-02-25 10:20:21 -0800

    do lab8

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,18 +10,38 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Page ready");
- 	// initCamera();
- 	// initMap();
+ 	initCamera();
+ 	initMap();
  	initGestures();
  	initRSVPForm();
 }
 
-// init jQuery gestures  
+// init jQuery gestures
 function initGestures() {
 	// add gestures listener here
+  $(function(){
+    $(".judge-img").bind("taphold", tapholdHandler);
+
+    function tapholdHandler(event){
+      var targetIDPrefix = event.target.id;
+      console.log("prefix:" + targetIDPrefix);
+      $("#" + targetIDPrefix + "-bio").show();
+    }
+  });
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
   // add your code here
+  $('#rsvpForm').submit(function(e){
+    e.preventDefault();
+    console.log("submitting");
+    var rsvpEmail = $('#rsvpEmail').val();
+    $.post('addRSVP', {rsvpEmail: rsvpEmail}, postCallback);
+  });
+
+  function postCallback(res) {
+    alert("RSVP form successfully submitted!");
+    $('#rsvpEmail').val('');
+  }
 }
--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,17 @@
 function initMap() {
 	// add your code here
+  L.mapquest.key = '8MiFcF9GImQQBbto5ZP70UaPPAGPI2Rd';
+
+  // 'map' refers to a <div> element with the ID map
+  var map = L.mapquest.map('map', {
+    center: [32.8785203, -117.2380747],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12,
+    zoomControl: false
+  });
+
+  L.marker([32.8785203, -117.2380747], {
+          icon: L.mapquest.icons.marker(),
+          draggable: false
+        }).addTo(map);
 }
